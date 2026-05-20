9eeaeec0cde242428591ee552ebdd2ba79087a7b
MathiasAAU <mkra17@student.aau.dk>
2026-05-20 22:21:47 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,28 +25,5 @@ export default defineNuxtConfig({
   runtimeConfig: {
     mongodbUri: process.env.MONGODB_URI,
     adminPassword: process.env.ADMIN_PASSWORD
-  },
-
-  pwa: {
-    manifest: {
-      name: 'Emil Mødeustabil',
-      short_name: 'Mødeustabil',
-      theme_color: '#020617',
-      background_color: '#020617',
-      display: 'standalone',
-
-      icons: [
-        {
-          src: '/icon-192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
   }
 })
