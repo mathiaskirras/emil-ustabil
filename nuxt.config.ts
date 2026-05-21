350dc8069501a81cedef7cb6b21a66a5543d5767
mathiaskirras <mathiaskirras@gmail.com>
2026-05-21 12:37:43 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,31 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt'
   ],
 
+  pwa: {
+    manifest: {
+      name: 'Emil Mødeustabil',
+      short_name: 'Mødeustabil',
+
+      theme_color: '#020617',
+      background_color: '#020617',
+
+      display: 'standalone',
+
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  },
+
   runtimeConfig: {
     mongodbUri: process.env.MONGODB_URI,
     adminPassword: process.env.ADMIN_PASSWORD
