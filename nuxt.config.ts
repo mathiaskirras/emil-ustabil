5a3592735246a4d2a88ec5ca6335742957a2c84a
mathiaskirras <mathiaskirras@gmail.com>
2026-05-21 12:39:46 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,29 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt'
   ],
 
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'apple-mobile-web-app-capable',
+          content: 'yes'
+        },
+        {
+          name: 'apple-mobile-web-app-status-bar-style',
+          content: 'black-translucent'
+        }
+      ],
+
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon.png'
+        }
+      ]
+    }
+  },
+
   pwa: {
     manifest: {
       name: 'Emil Mødeustabil',
