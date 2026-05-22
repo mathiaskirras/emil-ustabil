0798b3e0d509931c91f7d92d280385d66e479c67
mathiaskirras <mathiaskirras@gmail.com>
2026-05-22 10:00:49 +0200

    Update [id].patch.ts

--- server/api/[id].patch.ts
+++ server/api/[id].patch.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from 'mongodb'
-import { getDb } from '../../utils/mongodb'
+import { getDb } from '../utils/mongodb'
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
